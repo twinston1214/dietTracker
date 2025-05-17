733a7d1e46e30202f99e798004eeb1bb87bf5b98
Jack Winston <jwinston@dhcp-210-198.student-wlan.bowdoin.edu>
2025-05-17 16:10:36 -0400

    Final project commit

--- app/controllers/diet_tracker_controller.rb
+++ app/controllers/diet_tracker_controller.rb
@@ -3,10 +3,10 @@ class DietTrackerController < ApplicationController
   def index
     puts "------------------ In Index -----------------------"
     @allCalories = CalorieEntry.all
-    
+
     puts "# of calories = #{@allCalories.size}"
     # @allCalories = @allCalories.sort_by { |calorie_entry| [-calorie_entry.calories, calorie_entry.meal] }
-    
+
     @selected_date = params[:date].present? ? Date.parse(params[:date]) : Date.today
     @calories_today = CalorieEntry.where(eaten_on: @selected_date)
     @daily_goal = DailyGoal.find_or_initialize_by(date: @selected_date)
@@ -16,23 +16,25 @@ class DietTrackerController < ApplicationController
 
   def enterMeal
     puts "---------------- In Enter Meal ----------------------"
-    meal = params[:mealInput]
-    calories = params[:calorieInput].to_f
-    image = params[:imageInput]
-    eaten_on = Date.today
-
-    new_entry = CalorieEntry.new(meal: meal, calories: calories, image: image, eaten_on: eaten_on)
+    @calorie_entry = CalorieEntry.new(calorie_entry_params)
+    @calorie_entry.eaten_on ||= Date.today
 
     # map = { "meal" => meal, "calories" => calories }
     # new_entry = CalorieEntry.new(map)
-    respond_to do |format|
-      if new_entry.save
-        puts "Success!"
-        format.html { redirect_to diet_tracker_url }
-      else
-        format.html { redirect_to "/" } # can create an error page
-      end
+
+    if @calorie_entry.save
+      redirect_to root_path, notice: "Meal saved sucessfully"
+    else
+      @selected_date = @calorie_entry.eaten_on || Date.today
+      @daily_goal = DailyGoal.find_or_initialize_by(date: @selected_date)
+      @calories_today = CalorieEntry.where(eaten_on: @selected_date)
+      @totalCalories = @calories_today.sum(:calories)
+      render :index
     end
+  end
+
+  def calorie_entry_params
+    params.require(:calorie_entry).permit(:meal, :calories, :image, :eaten_on)
   end
 
   def delete
@@ -44,7 +46,7 @@ class DietTrackerController < ApplicationController
   def update_goal
     date = Date.parse(params[:date])
     goal = params[:daily_goal][:goal].to_f
-    
+
     goal_entry = DailyGoal.find_or_initialize_by(date: date)
     goal_entry.goal = goal
     goal_entry.save
--- app/models/calorie_entry.rb
+++ app/models/calorie_entry.rb
@@ -3,12 +3,13 @@ class CalorieEntry < ApplicationRecord
     validates :calories, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :meal, presence: true
     validate :meal_must_be_string
-    
+
     private
-    
+
     def meal_must_be_string
-        unless meal.match?(/[a-zA-Z]/)
-            errors.add(:meal, "must be a string")
+        meal_str = meal.to_s
+        if meal_str.blank? || !meal_str.match?(/[a-zA-Z]/)
+            errors.add(:meal, "must contain letters")
         end
     end
 end
